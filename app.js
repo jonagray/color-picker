5788bb72f07fc7cd8f10dc00129e2f9102b6b58c
Jonny Graybill <jon.a.graybill@gmail.com>
2021-03-27 15:04:40 -0700

    functional color picker

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"]; // Put all colors into this array when a set range of colors is desired
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+})
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * colors.length);
+}
